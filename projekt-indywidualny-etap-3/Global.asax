008fae80eb9e0c326a37212f6d61a0006cce1ff3
MarcinOsiadacz <marcin.osiadacz@outlook.com>
2021-06-06 10:12:59 +0200

    Initial commit.

---
+++ projekt-indywidualny-etap-3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projekt_indywidualny_etap_3.MvcApplication" Language="C#" %>
---
+++ projekt-indywidualny-etap-3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projekt_indywidualny_etap_3.Startup))]
+namespace projekt_indywidualny_etap_3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
