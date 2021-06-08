548905fe4186b23953b511deb003113cbe74eabb
MarcinOsiadacz <marcin.osiadacz@outlook.com>
2021-06-08 22:09:57 +0200

    Basic views added

---
+++ projekt-indywidualny-etap-3/Controllers/EventController.cs
@@ -0,0 +1,41 @@
+﻿using projekt_indywidualny_etap_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_indywidualny_etap_3.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            using (var context = ApplicationDbContext.Create())
+            {
+                var @events = context.Events
+                                .Include("EventType")
+                                .Include("EventCategory")
+                                .ToList();
+
+                return View(@events);
+            }
+        }
+
+        // GET: Event/Details/5
+        public ActionResult Details(int id)
+        {
+            using (var context = ApplicationDbContext.Create())
+            {
+                var @event = context.Events
+                                .Include("EventType")
+                                .Include("EventCategory")
+                                .Where(e => e.Id == id)
+                                .FirstOrDefault();
+
+                return View(@event);
+            }
+        }
+    }
+}
--- projekt-indywidualny-etap-3/Migrations/Configuration.cs
+++ projekt-indywidualny-etap-3/Migrations/Configuration.cs
@@ -4,20 +4,47 @@ namespace projekt_indywidualny_etap_3.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Models;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            context.Types.AddOrUpdate(new EventTypeModel { Name = "Face to Face" });
+            context.Types.AddOrUpdate(new EventTypeModel { Name = "Remote" });
+
+            context.Categories.AddOrUpdate(new EventCategoryModel { Name = "Computer Science" });
+            context.Categories.AddOrUpdate(new EventCategoryModel { Name = "Personal Health" });
+            context.Categories.AddOrUpdate(new EventCategoryModel { Name = "Games" });
+            context.Categories.AddOrUpdate(new EventCategoryModel { Name = "Books" });
+
+
+            /*  context.Events.AddOrUpdate(
+                    new EventModel
+                    {
+                        Title = "Test Event 1",
+                        Description = "Lorem Ipsum Lorem Ipsum...",
+                        OrganizedBy = "marcin.osiadacz",
+                        StartTime = DateTime.Now,
+                        EndTime = DateTime.Now.AddHours(3),
+                        Location = "Warsaw",
+                      EventTypeId = context.Types.FirstOrDefault(t => t.Name == "Face to Face").Id,
+                        EventType = context.Types.FirstOrDefault(t => t.Name == "Face to Face"),
+                        EventCategoryId = context.Categories.FirstOrDefault(c => c.Name == "Books").Id,
+                        EventCategory = context.Categories.FirstOrDefault(c => c.Name == "Books"), 
+            AvailableSlots = 10
+                    }
+                );*/
+
         }
     }
 }
